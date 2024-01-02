e748bdb9de3445015612d8b955052ac8a83a140d
Aly Hussein <111089237+alyHusseinn@users.noreply.github.com>
2024-01-03 00:22:53 +0200

    game page finished

--- src/pages/game.tsx
+++ src/pages/game.tsx
@@ -1,14 +1,20 @@
-import {  useLoaderData, Params } from "react-router-dom";
+import { useLoaderData, Params } from "react-router-dom";
 import { type GameType, getGameDetails } from "../dataFetching";
 import useUpdateTitle from "../hooks/useUpdateTitle";
 import GameSwiper from "../components/game/gameSwiper";
+import { useContext, useEffect, useRef } from "react";
+import { ContextType, MyContext } from "../Context/cartContext";
 
 // const gamePath = "/game/:gameId";
 // interface paramsType extends LoaderFunctionArgs {
 //   params: Params<ParamParseKey<typeof gamePath>>;
 // }
 
-export async function loader({params}: {params: Params<"gameId">}): Promise<GameType> {
+export async function loader({
+  params,
+}: {
+  params: Params<"gameId">;
+}): Promise<GameType> {
   const gameID = Number(params.gameId);
   const game = await getGameDetails(gameID);
   return game;
@@ -17,21 +23,49 @@ export async function loader({params}: {params: Params<"gameId">}): Promise<Game
 function Game() {
   const game: GameType = useLoaderData() as GameType;
   useUpdateTitle(game.name);
+  const { cart, setCart } = useContext(MyContext) as ContextType;
+  const gameRef = useRef<HTMLDivElement | null>(null);
+
+  const hasAdded = cart.includes(game.id);
+
+  function updateCart() {
+    if (cart.includes(game.id)) {
+      setCart(cart.filter((id: number) => id !== game.id));
+    } else {
+      // if we use cart.push(gam.id), react will not change the state
+      // because it compare the coming array using shallow equality
+      // so we need to set new array in order to be updated
+      setCart([...cart, game.id]);
+    }
+  }
+
+  useEffect(() => {
+    const header = document.querySelector("header") as HTMLDivElement;
+    const headerHeight = header.offsetHeight;
+    (gameRef.current as HTMLDivElement).style.paddingTop =
+      headerHeight + 10 + "px";
+  }, []);
 
   return (
-    <div className="game">
+    <div className="game" ref={gameRef}>
       <div className="game-name">{game.name}</div>
       <div className="game-info">
-        <GameSwiper imgs={game.short_screenshots}/>
-        <div className="info">
-          <div className="description">
-            {game.description}
-          </div>
+        <GameSwiper imgs={game.short_screenshots} />
+        <div className="price-and-add-btn">
+          <button className={hasAdded ? "added" : ""} onClick={updateCart}>
+            {hasAdded ? "Added" : "Add to cart+"}
+          </button>
+          <h3 className="price">$5.5</h3>
+        </div>
+        
+        <div className="description">
+          <span>Description: </span> {game.description}
+        </div><div className="release-geners">
           <div className="geners">
-           Geners: {game.genres.map(gen => gen.name + "  ")}
+            <span>Geners: </span> {game.genres.map((gen) => gen.name + "  ")}
           </div>
           <div className="released">
-            Release: {game.released}
+            <span>Release:</span> {game.released}
           </div>
         </div>
       </div>
